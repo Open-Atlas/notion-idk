947afea3b163025ce71d887699c43a255184f987
RayRemnant <rayremnant.me@gmail.com>
2022-08-27 14:35:07 +0000

    adding to-do for Notion pagination

--- notion/get/index.js
+++ notion/get/index.js
@@ -3,6 +3,7 @@ const format = require('./format.js');
 
 module.exports = async (dataType, {id=null, query=null, ...options}) => {
   const result = await get[dataType](id||query);
+  // TO-DO: start_cursor and next_cursor from Notion API, can wait to implement https://developers.notion.com/reference/pagination
   return options.raw ? result : await format(result, options);
 
   // caching does not seem to make much of a difference, but well, let's leave it here
